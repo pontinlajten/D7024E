33b0156c5d3a0c6b5bb85dc03ccdecf9000d0be4
pederj <peder96@gmail.com>
2021-09-21 13:17:06 +0200

    hashfunc and new kademlia
    2 funcs added

--- d7024e/kademlia.go
+++ d7024e/kademlia.go
@@ -1,9 +1,37 @@
 package d7024e
 
+import (
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+const (
+	k int = 20 // num of cont in bucket
+	a int = 3  //(alpha) degree of parallelism in network calls
+)
+
 type Kademlia struct {
-	me      Contact
-	table   *RoutingTable
-	network *Network
+	id *KademliaID
+	me Contact
+	rt *RoutingTable
+	//nt *Network
+}
+
+func NewKademlia(ip string) (kadNode Kademlia) {
+	//kademlia := Kademlia{}
+	kadNode.id = NewKademliaID(ip)
+	kadNode.me = NewContact(kadNode.id, ip)
+	kadNode.rt = NewRoutingTable(kadNode.me)
+	//kademlia.nt = createNetwork(me, rt, kademlia)
+	return
+}
+
+//help function that has data
+func hashIt(str string) (hash string) {
+	hashStr := sha1.New()
+	hashStr.Write([]byte(str))
+	hash = hex.EncodeToString(hashStr.Sum(nil))
+	return
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
