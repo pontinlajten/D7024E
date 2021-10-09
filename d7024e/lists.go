83bc665f85617f7660408471f54b16592c237a0d
pederj <peder96@gmail.com>
2021-10-09 20:27:16 +0200

    Update lists.go
    list upadte

--- d7024e/lists.go
+++ d7024e/lists.go
@@ -1,9 +1,39 @@
 package d7024e
 
+import (
+	"sync"
+)
+
+type Lookup struct {
+	Con   []Item
+	Mutex sync.Mutex
+}
+
+type List struct {
+	Cons  []Item
+	Mutex sync.Mutex
+}
+
+type Item struct {
+	Con  Contact
+	Seen bool
+}
+
 // creates New list with x closest
 func (kademlia *Kademlia) FindXClosest(target *Contact, x int) []Contact {
 	xClosest := kademlia.Rt.FindClosestContacts(target.ID, x)
 	return xClosest
+}
+
+func (kademlia *Kademlia) NewList(target *Contact) (list *List) {
+	list = &List{}
+	klist := kademlia.FindXClosest(target, bucketSize)
+
+	for _, item := range klist {
+		listitem := &Item{item, false}
+		list.Cons = append(list.Cons, *listitem)
+	}
+	return
 }
 
 func UpdateList() {
